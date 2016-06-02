a4a3bfb6a18ddf76124c21db33993d0680d8dee1
Lucia Velasco <lucia.velasco@mayden.co.uk>
2016-06-02 15:35:40 +0100

    initial commit

---
+++ js/models/ToDo.js
@@ -0,0 +1,8 @@
+// models/ToDo.js
+app.models.ToDo = Backbone.Model.extend({
+  defaults: {
+    title: "ToDo",
+    archived: false,
+    done: false
+  }
+});
---
+++ js/views/menu.js
@@ -0,0 +1,10 @@
+// views/menu.js
+app.views.menu = Backbone.View.extend({
+  template: _.template($("#tpl-menu").html()),
+  initialize: function() {
+    this.render();
+  },
+  render: function(){
+    this.$el.html(this.template({}));
+  }
+});
